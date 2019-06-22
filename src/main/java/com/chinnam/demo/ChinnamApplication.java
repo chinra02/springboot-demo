598906c2613b2d507b9a7d79f82dee92c7f43e70
chinra02 <raghavendrarao.chinnam@gmail.com>
2019-06-22 14:16:44 -0500

    init

---
+++ src/main/java/com/chinnam/demo/ChinnamApplication.java
@@ -0,0 +1,13 @@
+package com.chinnam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinnamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chinnam/demo/ChinnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinnam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
